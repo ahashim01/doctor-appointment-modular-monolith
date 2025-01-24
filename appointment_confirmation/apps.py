443fab590add694fa79261cd16f250186ac4507e
Ahmed Hashim <aahashim01@gmail.com>
2025-01-24 15:39:30 +0200

    Added modular apps: doctor_availability, appointment_booking, appointment_confirmation, appointment_management

---
+++ appointment_confirmation/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AppointmentConfirmationConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "appointment_confirmation"
--- doctor_appointment_app/settings.py
+++ doctor_appointment_app/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "doctor_availability",
+    "appointment_booking",
+    "appointment_confirmation",
+    "appointment_management",
 ]
 
 MIDDLEWARE = [
