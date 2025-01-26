7042ef83902d4b5180cc7cf8f5dc4665b81d7f6e
Ahmed Hashim <aahashim01@gmail.com>
2025-01-26 03:32:49 +0200

    Add Doctor and Slot models with migrations

--- doctor_availability/models.py
+++ doctor_availability/models.py
@@ -1,3 +1,24 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class Doctor(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    specialization = models.CharField(max_length=255, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Slot(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    time = models.DateTimeField()
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE, related_name="slots")
+    is_reserved = models.BooleanField(default=False)
+    cost = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.doctor.name} - {self.time}"
